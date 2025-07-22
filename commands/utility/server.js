c6220580c5a0d7dad21343a2531cf89e93f2b0f4
snek <snekom28@gmail.com>
2025-07-22 05:24:40 +0200

    Allows to run the bot

--- commands/utility/server.js
+++
@@ -1,9 +0,0 @@
-const {SlashCommandBuilder} = require("discord.js");
-
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("server"),
-    async execute(interaction) {
-        await interaction.reply(`this is ${interaction.guild.name}`);
-    },
-};
